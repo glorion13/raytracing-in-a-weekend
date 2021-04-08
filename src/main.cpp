7701494563c58b7e0451c417981ab2efd2a072fd
Alexandros Gouvatsos <a.gouvatsos@gmail.com>
2021-04-08 22:23:46 +0100

    Generate basic PPM image.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
-int main(int, char **)
+int main()
 {
-    std::cout << "Hello, world!\n";
+
+    // Image
+
+    const int image_width = 256;
+    const int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        std::cerr << "Progress (scanlines remaining): " << j << '\n';
+        for (int i = 0; i < image_width; ++i)
+        {
+            float r = float(i) / (image_width - 1);
+            float g = float(j) / (image_height - 1);
+            float b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }
